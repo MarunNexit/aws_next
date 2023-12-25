8d57d4db3e59a7f6b2f28419c87788673a4ffb1a
MarunNexit <mazkevych2003@gmail.com>
2023-12-25 05:03:57 +0200

    update 24.12 6

--- src/app/pages/editor/profile-editor/page.js
+++ src/app/pages/editor/profile-editor/page.js
@@ -5,37 +5,12 @@ import styles from "./page.module.css";
 import {GetAllUsersInfo} from "@/app/functions/GetAllUsersInfo";
 import {useEffect, useState} from "react";
 import {getUserInfo} from "@/app/functions/GetUserInfo";
-import {signIn} from "aws-amplify/auth";
 
 
 
 const ProfileEditor = () => {
     const [userInfo, setUserInfo] = useState([]);
 
-    useEffect( () => {
-        async function UserInfo() {
-            try {
-                const temp = await getUserInfo()
-                console.log(temp)
-
-                await setUserInfo(
-                    temp,
-                );
-
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        if (typeof window !== 'undefined') {
-            UserInfo()
-        } else {
-            // код, который выполнится только на сервере
-        }
-    }, []);
-
-    useEffect(() => {
-        console.log(userInfo)
-    }, [userInfo]);
 
 
     return (
