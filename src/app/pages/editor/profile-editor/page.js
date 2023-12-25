e0d1b681317bf360e8f635e50e5697a3284a1ee2
MarunNexit <mazkevych2003@gmail.com>
2023-12-25 05:53:49 +0200

    update 24.12 10

--- src/app/pages/editor/profile-editor/page.js
+++ src/app/pages/editor/profile-editor/page.js
@@ -1,47 +1,18 @@
-"use client"
+'use client'
 import Header from "../../../components/header";
 import ContainerForm4 from "../../../components/container-form4";
 import styles from "./page.module.css";
-import {GetAllUsersInfo} from "@/app/functions/GetAllUsersInfo";
 import {useEffect, useState} from "react";
 import {getUserInfo} from "@/app/functions/GetUserInfo";
 
 
 
 const ProfileEditor = () => {
-    const [userInfo, setUserInfo] = useState([]);
-
-    useEffect(() => {
-        async function UserInfo() {
-            try {
-                if (typeof window !== 'undefined') {
-                    const temp = await getUserInfo()
-                    console.log(temp)
-
-                    await setUserInfo(temp);
-                }
-            } catch (error) {
-                console.log(error)
-            }
-        }
-
-        UserInfo();
-    }, []);
-
-    useEffect(() => {
-        console.log(userInfo)
-    }, [userInfo]);
 
 
     return (
         <div className={styles.profileeditor}>
-            <Header value={"editor"} />
-            <div className={styles.profileeditorInner}>
-                <div className={styles.frameChild} />
-            </div>
-            <div className={styles.profileeditorChild}>
-                <ContainerForm4 userInfo = {userInfo}/>
-            </div>
+
         </div>
     );
 };
