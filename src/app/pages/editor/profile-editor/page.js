cc503e0291fd3a7f996ccdaec777cbdc23f09ff8
MarunNexit <mazkevych2003@gmail.com>
2023-12-25 05:12:28 +0200

    update 24.12 8

--- src/app/pages/editor/profile-editor/page.js
+++ src/app/pages/editor/profile-editor/page.js
@@ -11,17 +11,35 @@ import {getUserInfo} from "@/app/functions/GetUserInfo";
 const ProfileEditor = () => {
     const [userInfo, setUserInfo] = useState([]);
 
+    useEffect( () => {
+        async function UserInfo() {
+            try {
+                const temp = await getUserInfo()
+                console.log(temp)
+
+                await setUserInfo(
+                    temp,
+                );
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        if (typeof window !== 'undefined') {
+            UserInfo()
+        } else {
+            // код, который выполнится только на сервере
+        }
+    }, []);
+
+    useEffect(() => {
+        console.log(userInfo)
+    }, [userInfo]);
 
 
     return (
         <div className={styles.profileeditor}>
-            <Header value={"editor"} />
-            <div className={styles.profileeditorInner}>
-                <div className={styles.frameChild} />
-            </div>
-            <div className={styles.profileeditorChild}>
-                <ContainerForm4 userInfo = {userInfo}/>
-            </div>
+
         </div>
     );
 };
