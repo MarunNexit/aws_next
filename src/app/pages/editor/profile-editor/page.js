2754f848e96e13739f2c7a8bc663b5880f511895
MarunNexit <mazkevych2003@gmail.com>
2023-12-25 06:19:26 +0200

    update 24.12 14

--- src/app/pages/editor/profile-editor/page.js
+++ src/app/pages/editor/profile-editor/page.js
@@ -1,13 +1,48 @@
-'use client'
+"use client"
+import Header from "../../../components/header";
+import ContainerForm4 from "../../../components/container-form4";
+import styles from "./page.module.css";
+import {GetAllUsersInfo} from "@/app/functions/GetAllUsersInfo";
+import {useEffect, useState} from "react";
+import {getUserInfo} from "@/app/functions/GetUserInfo";
+import {signIn} from "aws-amplify/auth";
+
 
 
 const ProfileEditor = () => {
+    const [userInfo, setUserInfo] = useState([]);
 
+    useEffect( () => {
+        async function UserInfo() {
+            try {
+                const temp = await getUserInfo()
+                console.log(temp)
+
+                await setUserInfo(
+                    temp,
+                );
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        UserInfo()
+    }, []);
+
+    useEffect(() => {
+        console.log(userInfo)
+    }, [userInfo]);
 
 
     return (
-        <div >
+        <div className={styles.profileeditor}>
 
+            <div className={styles.profileeditorInner}>
+                <div className={styles.frameChild} />
+            </div>
+            <div className={styles.profileeditorChild}>
+                <ContainerForm4 userInfo = {userInfo}/>
+            </div>
         </div>
     );
 };
